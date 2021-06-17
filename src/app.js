4f434397f0a454d2ed7f0722972c1c5c8d428105
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-17 13:01:18 -0300

    começo da CRUD games

--- src/app.js
+++ src/app.js
@@ -27,14 +27,15 @@ app.post('/categories', async (req,res)=>{
         res.sendStatus(400);
         return;
     }
+    const categoryName = name.trim();
     try{
         const categories = await connection.query('SELECT name FROM categories');
         const categoriesArray = categories.rows.map((e)=>e.name);
-        if(categoriesArray.includes(name)){
+        if(categoriesArray.includes(categoryName)){
             res.sendStatus(409);
             return;
         }
-        await connection.query('INSERT INTO categories (name) values ($1)',[name]);
+        await connection.query('INSERT INTO categories (name) values ($1)',[categoryName]);
         res.send(201);
     }catch(error){
         console.log(error);
@@ -48,6 +49,50 @@ app.get('/categories', async (req,res)=>{
     res.send(allCategories.rows);
 })
 
+//CRUD Jogos
+//Create
+app.post('/games', async (req,res)=>{
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    if(!name?.trim()){
+        console.log(1);
+        res.sendStatus(400);
+        return;
+    }
+    if(!stockTotal || !pricePerDay){
+        console.log(2);
+        res.sendStatus(400);
+        return;
+    }
+    if(stockTotal<0 || pricePerDay<0){
+        console.log(3);
+        res.sendStatus(400);
+        return;
+    }
+    try{
+        const categories = await connection.query('SELECT id FROM categories');
+        const categoriesArray = categories.rows.map((e)=>e.id);
+        if(!categoriesArray.includes(categoryId)){
+            console.log(4);
+            res.sendStatus(400);
+            return;
+        }
+
+        const games = await connection.query('SELECT name FROM games');
+        const gamesArray = games.rows.map((e)=>e.name);
+        const gameName = name.trim();
+        if(gamesArray.includes(gameName)){
+            res.sendStatus(409);
+            return;
+        }
+
+        await connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") values ($1, $2, $3, $4, $5)`,[gameName, image, stockTotal, categoryId, pricePerDay]);
+        res.send(201);
+
+    }catch(error){
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
 
 
 
